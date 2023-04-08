706e5eb3a21042b1c95105eee41f9cd656bdd20e
Enzo <enzo.djabali@supinfo.com>
2023-04-08 03:02:46 +0200

    changing the sql file path (#59)

--- docker/scripts/migrate-database-prod.sh
+++ docker/scripts/migrate-database-prod.sh
@@ -1,4 +1,4 @@
 docker exec rotr_mysql mysql -uroot -pChangeMe -e "DROP DATABASE IF EXISTS rotr;"
 docker exec rotr_mysql mysql -uroot -pChangeMe -e "CREATE DATABASE rotr;"
-docker cp realm-of-the-righteous/docker/scripts/rotr.sql rotr_mysql:/
+docker cp realm-of-the-righteous/docker/rotr.sql rotr_mysql:/
 docker exec rotr_mysql mysql -uroot -pChangeMe -e "use rotr; SET autocommit=0; source rotr.sql; COMMIT;"
