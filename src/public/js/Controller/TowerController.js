948c6133142e2236c2c6f1d6a8a1dda3c72edba4
Gabriel Titeux <72764917+Xelone28@users.noreply.github.com>
2023-04-10 17:21:15 +0200

    implement tower verify his neighbour

--- src/public/js/Controller/TowerController.js
+++ src/public/js/Controller/TowerController.js
@@ -8,7 +8,6 @@ export class TowerController{
     placeTowerInMatrice(){
         this.display.pile[0].classList.remove('tile-shadow'); // remove class (not selected anymore)
         if(this.model.matrice[this.display.pile[1][0]][this.display.pile[1][1]].tower == null){
-            console.log(this.model.towerId, 'id tower ici');
             let towerId = 'tower'+this.model.towerId;
             this.model.towerId++;
             this.model.matrice[this.display.pile[1][0]][this.display.pile[1][1]].tower = new Tower(
@@ -26,14 +25,28 @@ export class TowerController{
         this.display.pile = -1;
     }
     async runTower(tower){
+        let DIRECTIONS = [[0, -1], [1, 0], [0, 1], [-1, 0], [-1,-1], [-1,1], [1,-1], [1,1]]; // North, East, South, West
         while(true){
             await new Promise(r => setTimeout(r, 200)); // frequence de tire
             // console.log(tower.position.x-1, tower.position.y-1, 'POSITION TOUR -1')
             // IMPLEMENTER LE FAIT DE REGARDER LE PERIMETRE
-            if(this.model.matrice[tower.position.x-1][tower.position.y-1].enemies.length > 0){
-                this.model.matrice[tower.position.x-1][tower.position.y-1].enemies[0].life = 0;
+            for (let direction of DIRECTIONS){
+                let dx = tower.position.x + direction[0]
+                let dy = tower.position.y + direction[1]
+                console.log(direction, '<<<<<<<<<<')
+                if(dx >= 0 && dx < this.model.matrice.length && dy >= 0 && dy < this.model.matrice[0].length){
+                    if(this.model.matrice[dx][dy].enemies.length > 0){
+                        console.log('enemy a range de tour')
+                        console.log(this.model.matrice[dx][dy], 'here')
+                        this.model.matrice[dx][dy].enemies[0].life = 0;
+
+                    }
+                }
             }
+            console.log('stop')
         }
     }
-
 }
+
+
+
