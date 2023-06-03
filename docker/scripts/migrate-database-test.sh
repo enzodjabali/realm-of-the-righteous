e58b389e29320f49093663500b3e1d1b06d16e1a
Enzo DJABALI <enzo.djabali@supinfo.com>
2023-06-04 00:47:00 +0200

    setting up the development password (#184)

--- docker/scripts/migrate-database-test.sh
+++ docker/scripts/migrate-database-test.sh
@@ -1,4 +1,4 @@
-docker exec rotr_mysql mysql -uroot -pChangeMe -e "DROP DATABASE IF EXISTS rotr_test;"
-docker exec rotr_mysql mysql -uroot -pChangeMe -e "CREATE DATABASE rotr_test;"
+docker exec rotr_mysql mysql -uroot -pdev -e "DROP DATABASE IF EXISTS rotr_test;"
+docker exec rotr_mysql mysql -uroot -pdev -e "CREATE DATABASE rotr_test;"
 docker cp ./docker/rotr.sql rotr_mysql:/
-docker exec rotr_mysql mysql -uroot -pChangeMe -e "use rotr_test; SET autocommit=0; source rotr.sql; COMMIT;"
+docker exec rotr_mysql mysql -uroot -pdev -e "use rotr_test; SET autocommit=0; source rotr.sql; COMMIT;"
