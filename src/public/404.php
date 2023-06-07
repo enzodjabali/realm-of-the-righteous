dff3224cef4e96649c5099e73f0b8ef37d8015cd
Enzo DJABALI <enzo.djabali@supinfo.com>
2023-06-07 16:45:00 +0200

    adding a 404 page (#220)

--- src/public/404.php
+++ src/public/404.php
@@ -7,15 +7,32 @@
         <link rel="icon" type="image/x-icon" href="assets/images/website/favicon.ico">
 
         <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.css">
+<<<<<<< main
     </head>
 
     <body>
+=======
+        <script src="node_modules/bootstrap/dist/js/bootstrap.js"></script>
+        <link rel="stylesheet" href="node_modules/bootstrap-icons/font/bootstrap-icons.min.css">
+    </head>
+
+    <body>
+        <?php include_once("includes/menu.php") ?>
+
+>>>>>>> adding a 404 page (#220)
         <div class="card text-center w-75 position-absolute top-50 start-50 translate-middle">
             <div class="card-body mt-3">
                 <h3>404</h3>
                 <p>Oops.. page not found!</p>
+<<<<<<< main
                 <a href="/">Back to the landing page</a>
             </div>
         </div>
+=======
+            </div>
+        </div>
+
+        <?php include_once("includes/footer.php") ?>
+>>>>>>> adding a 404 page (#220)
     </body>
 </html>
