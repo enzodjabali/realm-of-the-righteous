4708b1d7fcfc7fbdd024c8ed1c3b5b1ac7f986a5
Enzo DJABALI <enzo.djabali@supinfo.com>
2023-06-08 11:32:01 +0200

    fixing the 404 page (#220)

--- src/public/404.php
+++ src/public/404.php
@@ -1,7 +1,3 @@
-<?php
-    session_start();
-?>
-
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -11,20 +7,15 @@
         <link rel="icon" type="image/x-icon" href="assets/images/website/favicon.ico">
 
         <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.css">
-        <script src="node_modules/bootstrap/dist/js/bootstrap.js"></script>
-        <link rel="stylesheet" href="node_modules/bootstrap-icons/font/bootstrap-icons.min.css">
     </head>
 
     <body>
-        <?php include_once("includes/menu.php") ?>
-
         <div class="card text-center w-75 position-absolute top-50 start-50 translate-middle">
             <div class="card-body mt-3">
                 <h3>404</h3>
                 <p>Oops.. page not found!</p>
+                <a href="/">Back to landing page</a>
             </div>
         </div>
-
-        <?php include_once("includes/footer.php") ?>
     </body>
 </html>
